b688e2e62e6bd3ce57b27ab416fbcfbf50f5cdf4
David Vergaray <david.vergaray.a@gmail.com>
2022-05-17 17:59:37 -0400

    Add recipe_food methods

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -3,4 +3,16 @@ class RecipeFood < ActiveRecord::Base
   belongs_to :recipe, class_name: 'Recipe'
 
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def value
+    "$#{quantity * food.price}"
+  end
+
+  def name
+    food.name
+  end
+
+  def quantity_measure
+    "#{quantity} #{food.measurement_unit}"
+  end
 end
