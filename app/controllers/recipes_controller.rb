79069f520fa4178bfab5b15947e735987bb5fee5
David Vergaray <david.vergaray.a@gmail.com>
2022-05-16 21:38:29 -0400

    Add recipe page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,10 @@
 class RecipesController < ApplicationController
-  def index; end
+  def index
+    @recipes = Recipe.where(user_id: current_user.id)
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+  
 end
