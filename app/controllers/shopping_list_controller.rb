b5f95dce400a045ba9bd5043a7fd6ea41c53da00
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-05-18 16:15:11 -0300

    create shopping list functionality

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,3 +1,13 @@
 class ShoppingListController < ApplicationController
-  def index; end
+  def index
+    @foods = Food.select('foods.id, foods.name,foods.price,
+      foods.measurement_unit, sum(recipe_foods.quantity) as quantity_sum').joins(:recipe_foods).group('foods.id')
+    @total_value = 0
+    @total_items = 0
+    @foods.each do |f|
+      # puts f.name, f.quantity_sum ,f.price, f.measurement_unit
+      @total_value += f.price * f.quantity_sum
+      @total_items += 1
+    end
+  end
 end
